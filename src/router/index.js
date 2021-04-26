fd413738c3658c6d396dd98d9c543ebcdff87248
sakuraliqi <358983697@qq.com>
2021-04-26 23:01:27 +0800

    Squashed commit of the following:
    commit b58a5922eaa55711f87fa079af260f9aaa956589
    Author: sakuraliqi <358983697@qq.com>
    Date:   Mon Apr 26 23:00:20 2021 +0800
    
        Squashed commit of the following:
    
        commit 3656c922cb1506414090635d96c786fb8137151e
        Merge: 3140c4b 1bb7aa1
        Author: sakuraliqi <358983697@qq.com>
        Date:   Mon Apr 26 22:59:14 2021 +0800
    
            Merge branch 'master' of https://github.com/sakuraliqi/vue-demo
    
        commit 3140c4b5a0c2bd4a7f783ade9eaa88546359df36
        Author: sakuraliqi <358983697@qq.com>
        Date:   Mon Apr 26 22:53:10 2021 +0800
    
            111
    
        commit 1bb7aa17ef1e9d126d4d02fb4250f9e07287d6ea
        Merge: 8d04519 a97ee9f
        Author: sakuraliqi <36870176+sakuraliqi@users.noreply.github.com>
        Date:   Mon Apr 26 22:47:41 2021 +0800
    
            Merge pull request #1 from sakuraliqi/develop_li
    
            修改后提交“

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@
  * @Author: liqi
  * @Date: 2021-04-22 20:48:33
  * @LastEditors: sueRimn
- * @LastEditTime: 2021-04-26 22:50:29
+ * @LastEditTime: 2021-04-26 23:01:16
  */
 //引入vue
 import Vue from 'vue';
@@ -22,7 +22,7 @@ const routes = [
     { path: '/hbTable', component: resolve => (require(["@/view/hbTable.vue"], resolve)) },
 ]
 console.log("1111");
-console.log("主分支开发的");
+console.log("主分支开发的123");
 //实例化VueRouter并将routes添加进去
 const router = new VueRouter({
     //ES6简写，等于routes：routes
